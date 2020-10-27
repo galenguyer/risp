27a171b78b8c99998c7c61cbec9c39849ddbc0ff
Galen Guyer <galen@galenguyer.com>
2020-10-26 21:37:52 -0400

    Add basic REPL loop with rustyline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
 fn main() {
-    println!("Hello, world!");
+    // create rustyline editor with no completer
+    let mut rl = Editor::<()>::new();
+
+    loop {
+        let readline = rl.readline("risp> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                println!("{}", line);
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("^C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("^D");
+                break;
+            }
+            Err(err) => {
+                println!("error: {}", err);
+            }
+        }
+    }
 }
